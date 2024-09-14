d1c3d63b8f865766b68f29632aebe44eeffc2778
jenna <shinysuh1992@gmail.com>
2024-09-14 22:43:07 +0900

    fix: mybatis 연결 오류 fix

--- src/main/java/com/jenna/pennypilot/core/security/config/MyBatisConfig.java
+++ src/main/java/com/jenna/pennypilot/core/security/config/MyBatisConfig.java
@@ -4,21 +4,36 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
 import javax.sql.DataSource;
 
 @Configuration
-@MapperScan(basePackages = "com.jenna.pennypilot.**.mapper")
+@MapperScan(basePackages = "com.jenna.pennypilot.domain.*.mapper")
 public class MyBatisConfig {
 
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
         sessionFactory.setDataSource(dataSource);
+        sessionFactory.setVfs(SpringBootVFS.class);
 
-        // MyBatis configuration without mybatis-config.xml
+        // 설정 검토
+        sessionFactory.setTypeHandlersPackage("com.commax.tool.framework.mybatis.typehandler");
+
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resolver.getResources("classpath:sql/*.xml");
+        sessionFactory.setMapperLocations(resources);
+
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        sessionFactory.setConfiguration(configuration);
+
         return sessionFactory.getObject();
     }
 
@@ -27,5 +42,6 @@ public class MyBatisConfig {
         return new SqlSessionTemplate(sqlSessionFactory);
     }
 }
+
 
 
