140e397a14ac264d7b9044ba1bfe6c37bf3c8e1e
jenna <shinysuh1992@gmail.com>
2024-10-24 16:36:38 +0900

    perf(FE/CategoryPieChart): 카테고리별 수입/지출 데이터 파이차트 적용

--- src/main/webapp/src/components/dashboard/custom/CategoryPieChart.tsx
+++ src/main/webapp/src/components/dashboard/custom/CategoryPieChart.tsx
@@ -4,7 +4,7 @@ import CardContent from '@mui/material/CardContent'
 import Chip from '@mui/material/Chip'
 import Typography from '@mui/material/Typography'
 import Stack from '@mui/material/Stack'
-import { styled, useTheme } from '@mui/material/styles'
+import { styled } from '@mui/material/styles'
 import { PieChart } from '@mui/x-charts/PieChart'
 import { useDrawingArea } from '@mui/x-charts/hooks'
 import {
@@ -12,33 +12,19 @@ import {
   TransactionType,
 } from '../../../types/TransactionType.ts'
 import Grid from '@mui/material/Grid2'
+import { useGetCtgTotalByPeriodMutation } from '../../../services/TransactionService.ts'
+import {
+  IPeriodParam,
+  ITotalByCtg,
+  ITotalByPeriod,
+} from '../../../interfaces/TransactionInterface.ts'
+import { compactNumberFormatter } from '../../../utils/NumberFormatter.ts'
 
 type CategoryPieChartProperties = {
   periodParams: IPeriodParam
 }
 
 const CategoryPieChart = (props: CategoryPieChartProperties) => {
-  const theme = useTheme()
-  const colorPalette = [
-    (theme.vars || theme).palette.primary.dark,
-    (theme.vars || theme).palette.primary.main,
-    (theme.vars || theme).palette.primary.light,
-  ]
-
-  const colors = [
-    'hsl(220, 20%, 65%)',
-    'hsl(220, 20%, 42%)',
-    'hsl(220, 20%, 35%)',
-    'hsl(220, 20%, 25%)',
-  ]
-
-  const data = [
-    { label: 'India', value: 50000 },
-    { label: 'USA', value: 35000 },
-    { label: 'Brazil', value: 10000 },
-    { label: 'Other', value: 5000 },
-  ]
-
   interface StyledTextProps {
     variant: 'primary' | 'secondary'
   }
@@ -103,41 +89,102 @@ const CategoryPieChart = (props: CategoryPieChartProperties) => {
     )
   }
 
-  const getPieChartByTransactionType = (type: string): React.JSX.Element => {
-    return (
-      <Grid size={{ md: 6 }}>
-        <PieChart
-          colors={colors}
-          margin={{
-            left: 0,
-            right: 0,
-            top: 40,
-            bottom: 40,
-          }}
-          series={[
-            {
-              data,
-              innerRadius: 60,
-              outerRadius: 90,
-              paddingAngle: 0,
-              highlightScope: { faded: 'global', highlighted: 'item' },
-            },
-          ]}
-          height={260}
-          width={215}
-          slotProps={{
-            legend: { hidden: true },
-          }}
-        >
-          <PieCenterLabel primaryText="98.5K" secondaryText="Total" />
-        </PieChart>
-      </Grid>
-    )
+  const [totalByTransactionType, setTotalByTransactionType] =
+    React.useState<ITotalByPeriod>({
+      transactionPeriod: '',
+      transactions: [],
+    })
+
+  const [getCtgTotalByPeriod] = useGetCtgTotalByPeriodMutation()
+
+  React.useEffect(() => {
+    getCtgTotalByPeriod(props.periodParams)
+      .unwrap()
+      .then((ctgTotal) => {
+        if (!!ctgTotal) setTotalByTransactionType(ctgTotal)
+      })
+  }, [props.periodParams])
+
+  const getColors = (type: string) => {
+    const lightness = 50
+
+    return type === TransactionType.INCOME
+      ? [
+          `hsl(212,${lightness}%,65%)`,
+          `hsl(205,${lightness}%,42%)`,
+          `hsl(210,${lightness}%,35%)`,
+          `hsl(203,${lightness}%,25%)`,
+        ]
+      : type === TransactionType.EXPENSE
+        ? [
+            `hsl(0,${lightness}%,65%)`,
+            `hsl(0,${lightness}%,42%)`,
+            `hsl(0,${lightness}%,35%)`,
+            `hsl(0,${lightness}%,25%)`,
+          ]
+        : [
+            'hsl(220, 20%, 65%)',
+            'hsl(220, 20%, 42%)',
+            'hsl(220, 20%, 35%)',
+            'hsl(220, 20%, 25%)',
+          ]
   }
 
+  const getPieChartByTransactionType = React.useCallback(
+    (type: string): React.JSX.Element => {
+      let total: string
+
+      const target: ITotalByCtg[] =
+        totalByTransactionType.transactions.find((tra) => {
+          total = compactNumberFormatter.format(tra.totalAmount) || '0'
+          return tra.transactionType === type
+        })?.ctgTotals || []
+
+      const data = target.map((ctgTotal) => {
+        return {
+          label: ctgTotal.ctgNm,
+          value: ctgTotal.totalAmount,
+        }
+      })
+
+      return (
+        <Grid size={{ md: 6 }}>
+          <PieChart
+            colors={getColors(type)}
+            margin={{
+              left: 0,
+              right: 0,
+              top: 40,
+              bottom: 40,
+            }}
+            series={[
+              {
+                data,
+                innerRadius: 60,
+                outerRadius: 90,
+                paddingAngle: 0,
+                highlightScope: { faded: 'global', highlighted: 'item' },
+              },
+            ]}
+            height={260}
+            width={215}
+            slotProps={{
+              legend: { hidden: true },
+            }}
+          >
+            <PieCenterLabel
+              primaryText={total}
+              secondaryText={type.toUpperCase()}
+            />
+          </PieChart>
+        </Grid>
+      )
+    },
+    [totalByTransactionType],
+  )
+
   const getPeriodToCompare = React.useCallback((): string => {
-    console.log(props.periodType)
-    switch (props.periodType) {
+    switch (props.periodParams.periodType) {
       case TransactionPeriodType.YEAR:
         return '전년'
       case TransactionPeriodType.MONTH:
@@ -147,7 +194,7 @@ const CategoryPieChart = (props: CategoryPieChartProperties) => {
       default:
         return '전월'
     }
-  }, [props.periodType])
+  }, [props.periodParams.periodType])
 
   return (
     <Card variant="outlined" sx={{ width: '100%' }}>
