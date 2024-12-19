62e37407c0034a21a11d4dd39c524bbe412895fb
jenna <shinysuh1992@gmail.com>
2024-12-19 16:59:24 +0900

    feat(PeriodParamDTO): @Builder annotation 추가

--- src/main/java/com/jenna/pennypilot/domain/transaction/dtos/PeriodParamDTO.java
+++ src/main/java/com/jenna/pennypilot/domain/transaction/dtos/PeriodParamDTO.java
@@ -1,9 +1,14 @@
 package com.jenna.pennypilot.domain.transaction.dtos;
 
-import com.jenna.pennypilot.domain.transaction.constants.PeriodType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PeriodParamDTO {
 
     private int userId;
