0ac03558c064d8388da6fc305c6674a49696c22c
jenna <shinysuh1992@gmail.com>
2024-12-26 15:09:38 +0900

    test(TestTransactionDTO): 테스트 DTO 필드 추가

--- src/test/java/com/jenna/pennypilot/dto/TestTransactionDTO.java
+++ src/test/java/com/jenna/pennypilot/dto/TestTransactionDTO.java
@@ -1,6 +1,10 @@
 package com.jenna.pennypilot.dto;
 
 import com.jenna.pennypilot.domain.transaction.dtos.TransactionResultDTO;
+import com.jenna.pennypilot.domain.transaction.dtos.ctg.TotalByCtgDTO;
+import com.jenna.pennypilot.domain.transaction.dtos.ctg.TotalByPeriodDTO;
+import com.jenna.pennypilot.domain.transaction.dtos.ctg.TotalByTypeDTO;
+import com.jenna.pennypilot.domain.transaction.dtos.monthly.DailyTransactionDTO;
 import lombok.Data;
 
 import java.util.List;
@@ -8,4 +12,12 @@ import java.util.List;
 @Data
 public class TestTransactionDTO {
     List<TransactionResultDTO> transactions;
+
+    List<DailyTransactionDTO> dailyTransactions;
+
+    List<TotalByTypeDTO> typeTotals;
+
+    TotalByPeriodDTO totalByPeriod;
+
+    List<TotalByCtgDTO> ctgTotals;
 }
